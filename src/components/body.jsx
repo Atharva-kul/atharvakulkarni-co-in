f51dc3b1932aa5977a2191a8e86b3b96094c0182
Atharva kulkarni <atharvakulkarni2112005@gmail.com>
2026-09-02 23:13:56 +0530

    adding glass-1

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -7,31 +7,32 @@ function Body() {
     return (
         <div className="relative flex w-full h-[650px] bg-[#FFF8ED] border-b-2">
         <div className="ml-[3rem] pt-[2rem] tracking-widest h-full">
-            <span className="font-pixel font-semibold text-shadow-lg [-webkit-text-stroke:1.5px_black] text-[64px] text-[black] leading-[0.98]">I  TEACH <br /> MACHINES  TO<br /> THINK </span> <br />
+            <span className="font-pixel font-semibold text-shadow-lg [-webkit-text-stroke:1.5px_black] text-[64px] text-[black] leading-[0.98]">I  TEACH MACHINES<br />   TO   THINK<br /> </span> <br />
             <span className="font-pixel text-[48px] text-[#11FF00] text-shadow-lg [-webkit-text-stroke:1px_black]">WELL... KIND OF</span> <br />
             <span className="text-[20px] text-[white] [-webkit-text-stroke:0.9px_black] text-shadow-lg">Building intelligent systems using AI/ML, <br /> Building websites, <br /> Building games</span>
 
-            <div className="w-full h-[180px] border mt-[2.5%] shadow-[4px_4px_0_0_#000] bg-[#F1E0C4]/30">
+            <div className="w-full h-[180px] border mt-[5%] shadow-[4px_4px_0_0_#000] bg-[#F1E0C4]/50">
 
-                    <div className="w-full h-[30%] border-b flex items-center">
-                        <span className="font-pixel ml-[7.5%] text-[30px] text-[#11FF00] flex items-center [-webkit-text-stroke:0.9px_black] text-shadow-lg">
-                            Featured Project
-                        </span>
-                    </div>
-                    <div className="flex items-center justify-between px-[7.5%] font-pixel h-[70%]">
-                        <span className="text-[40px] font-bold text-white [-webkit-text-stroke:0.9px_black] whitespace-nowrap">
-                            CHESS BOT
-                        </span>
-                        <a className="text-[#11FF00] text-[32px] [-webkit-text-stroke:0.9px_black] whitespace-nowrap" href="#">
+                <div className="w-full h-[30%] border-b flex items-center">
+                    <span className="font-pixel ml-[7.5%] text-[30px] text-[#11FF00] flex items-center [-webkit-text-stroke:0.9px_black] text-shadow-lg">
+                        Featured Project
+                    </span>
+                </div>
+                <div className="flex items-center justify-between px-[7.5%] font-pixel h-[70%]">
+                    <span className="text-[40px] font-bold text-white [-webkit-text-stroke:0.9px_black] whitespace-nowrap">
+                        CHESS BOT
+                    </span>
+                    <a className="text-[#11FF00] text-[32px] [-webkit-text-stroke:0.9px_black] whitespace-nowrap" href="#">
                             GitHub
-                        </a>
+                    </a>
 
-                    </div>
+                </div>
+            </div>
             
         </div>
-        <div className="absolute z-20 ml-142 -mt-[450px]">
+        <div className="absolute z-20 ml-[45rem] mt-[45px]">
             <GlassCard />
-        </div>
+        
         </div>
         </div>
         
--- src/components/glassCard.jsx
+++ src/components/glassCard.jsx
@@ -4,12 +4,24 @@ function GlassCard() {
     return(
         <div className="w-[300px]
             h-[450px]
-            rounded-3xl
-            bg-white/20
-            backdrop-blur-xl
-            border
+            [-webkit-text-stroke:1px_black]
+            bg-[#E8E7E7]/50
+            
+            border-b-2
             border-black/40
-            shadow-xl"> hello world</div>
+            shadow-xl">
+                <div className="flex flex-col items-center justify-center ">
+                    <div className="font-pixel text-[#FFEE00] text-[28px] mt-[3.2rem]">AI/ML</div>
+                    <div className="text-[18px] text-white">Building intelligent models</div>
+
+                    <div className="font-pixel text-[#FFEE00] text-[28px] mt-[3.5rem]">Developer</div>
+                    <div className="text-[18px] text-white">Frontend-Backend</div>
+
+                    <div className="font-pixel text-[#FFEE00] text-[28px] mt-[3.5rem]">Game Dev</div>
+                    <div className="text-[18px] text-white">Godot and game logic</div>
+                </div>
+                
+            </div>
     )
 }
 
